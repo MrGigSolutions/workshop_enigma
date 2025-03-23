ab42d0d6eaf8220ac6feaf926d88f6441071b292
Mark Schuit <mark@gig.solutions>
2025-03-23 20:32:45 +0100

    Set up as a copier template

---
+++ main.py
@@ -0,0 +1,6 @@
+# from keyboard_clan import keyboard
+# from wire_warriors.machine import Enigma
+#
+# if __name__ == "__main__":
+#     machine = Enigma()
+#     keyboard.run(machine, input, print)
